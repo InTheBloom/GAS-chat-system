bfb1acb9bdc7fb8ae2e01087ca6e97ad3d58a5f6
InTheBloom <nato.rider.smm2@gmail.com>
2024-11-27 20:21:39 +0900

    GAS側に変更を入れた。今後の予定を考えた

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,5 @@
 /* Variables */
-const gas_app_url = "https://script.google.com/macros/s/AKfycby3ULOtwui-rf5qfvb6EGckRDePRj764zVcLS_WiXHZFr5usbFPCAPy51rbv_tiAZ6wKA/exec";
+const gas_app_url = "https://script.google.com/macros/s/AKfycbz-i6N-U_QAHzOzu-SEC-NM9bX2zygnLygnM5ShyLS-8c4flzUCFPNx944Mc61O_bwGjg/exec";
 
 const main_page = "/";
 const forum_page = "/forum/";
