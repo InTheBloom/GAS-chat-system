aef6a1f9de6d845b64c925c670c7c61629e7fd54
InTheBloom <nato.rider.smm2@gmail.com>
2024-11-28 18:01:54 +0900

    細かい調整など

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,5 @@
 /* Variables */
-const gas_app_url = "https://script.google.com/macros/s/AKfycbxg0qSalnOTNJXGHe16AWtqWw-oNgyMhnLXz6_UptxS10S8T09CbqikvQN9DNRGPq9KKw/exec";
+const gas_app_url = "https://script.google.com/macros/s/AKfycbynlgnc3xohurJaZl8hFcNId0cDkYR_e2_wd15oa5N8vcdcbnCcLheWwjLRe90Jl4Qu2g/exec";
 
 const main_page = "/";
 const forum_page = "/forum/";
